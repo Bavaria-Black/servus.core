0c7aaf01e43774110bb977ba5a06ab10639731f3
Christian Dirnhofer <cdirnhofer@serva-ts.com>
2019-09-15 13:06:54 +0200

    initial commit

---
+++ src/DevTools.Core.Tests/Encoding/ModHexTests.cs
@@ -0,0 +1,25 @@
+﻿using DevTools.Core.Encoding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTools.Core.Tests.Encoding
+{
+    [TestClass]
+    public class ModHexTests
+    {
+        [TestMethod]
+        public void Encode()
+        {
+            var result = ModHex.EncodeFromAscii("test");
+            Assert.AreEqual("ifhgieif", result);
+        }
+        [TestMethod]
+        public void Decode()
+        {
+            var result = ModHex.DecodeToAscii("ifhgieif");
+            Assert.AreEqual("test", result);
+        }
+    }
+}
---
+++ src/DevTools.Core/Encoding/ModHex.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTools.Core.Encoding
+{
+    public class ModHex
+    {
+        private static readonly char[] _alphabet =
+        {
+            //0   1    2   3   4   5   6   7   8   9  a    b   c   d   e   f
+            'c', 'b', 'd','e','f','g','h','i','j','k','l','n','r','t','u','v'
+        };
+
+        public static string EncodeFromAscii(string ascii)
+        {
+            var bytes = System.Text.Encoding.ASCII.GetBytes(ascii);
+            return Encode(bytes);
+        }
+
+        public static string Encode(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                var i1 = (bytes[i] >> 4) & 0xf;
+                var i2 = bytes[i] & 0xf;
+
+                builder.Append(_alphabet[i1]);
+                builder.Append(_alphabet[i2]);
+            }
+
+            return builder.ToString();
+        }
+
+        public static string DecodeToAscii(string modhex)
+        {
+            var bytes = Decode(modhex);
+            return System.Text.Encoding.ASCII.GetString(bytes);
+        }
+
+        public static byte[] Decode(string modhex)
+        {
+            if (modhex.Length % 2 != 0)
+            {
+                throw new ArgumentException("modhex contains an invalid encoding");
+            }
+
+            byte[] value = new byte[modhex.Length / 2];
+            for (int i = 0; i < modhex.Length; i = i + 2)
+            {
+                var index1 = Array.IndexOf(_alphabet, modhex[i]);
+                var index2 = Array.IndexOf(_alphabet, modhex[i + 1]);
+                value[i / 2] = (byte)(index1 << 4 | index2);
+            }
+
+            return value;
+        }
+    }
+}
