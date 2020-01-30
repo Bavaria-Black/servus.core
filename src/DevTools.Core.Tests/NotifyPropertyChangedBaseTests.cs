cb15d3ad15f64aa1b8dd8169ecb5de84584a97d8
Christian Dirnhofer <cdirnhofer@serva-ts.com>
2020-01-30 21:49:07 +0100

    added NotifyPropertyChangedBase

---
+++ src/DevTools.Core.Tests/NotifyPropertyChangedBaseTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTools.Core.Tests
+{
+    [TestClass]
+    public class NotifyPropertyChangedBaseTests
+    {
+        [TestMethod]
+        public void PropertyChangedTest()
+        {
+            bool changed = false;
+            var testclass = new TestClass();
+            testclass.PropertyChanged += (s, e) =>
+            {
+                changed = true;
+            };
+
+            testclass.Property = true;
+            Assert.IsTrue(changed);
+        }
+        [TestMethod]
+        public void PropertyNotChangedTest()
+        {
+            bool changed = false;
+            var testclass = new TestClass();
+            testclass.PropertyChanged += (s, e) =>
+            {
+                changed = true;
+            };
+
+            testclass.Property = false;
+            Assert.IsFalse(changed);
+
+            testclass.Property = true;
+            Assert.IsTrue(changed);
+        }
+
+        public class TestClass : NotifyPropertyChangedBase
+        {
+            private bool _property;
+            public bool Property { get => _property; set => ChangeProperty(value, ref _property); }
+        }
+    }
+}
---
+++ src/DevTools.Core/NotifyPropertyChangedBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DevTools.Core
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void ChangeProperty<T>(T value, ref T target, [CallerMemberName] string caller = "")
+        {
+            if (!value.Equals(target))
+            {
+                target = value;
+                OnPropertyChanged(caller);
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
