88492085efa19dcac04d2aae2dc5fee00d89cba2
Andreas Huber <infinite-coding@outlook.de>
2020-02-06 19:07:56 +0100

    Create empty eventbus class and test

---
+++ src/DevTools.Core.Tests/Events/EventBus.cs
@@ -0,0 +1,29 @@
+﻿using DevTools.Core.Events;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevTools.Core.Tests.Events
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        private EventBus _eventBus;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _eventBus = new EventBus();
+        }
+
+        [TestMethod]
+        public void PublishWithoutSubscribersDoesNothing()
+        {
+            _eventBus.Publish("/food", "Payload: Leberkas Semme");
+        }
+
+        [TestMethod]
+        public void PublishWithSingleSubscriberIsReceived()
+        {
+            
+        }
+    }
+}
---
+++ src/DevTools.Core/Events/EventBus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DevTools.Core.Events
+{
+    //ToDo: Is the folder events alright?
+    //todo:  Publish T?
+    //todo: Subscribe to multiple T on same topic
+    public class EventBus
+    {
+        
+        
+        public void Publish<T>(string topic, T message)
+        {
+            
+        }
+
+        public void Subscribe<T>(Action<T> action)
+        {
+            
+        }
+    }
+}
