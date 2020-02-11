296f0649dd24c30e7fc41aa3ea4ce05dc5f05027
Andreas Huber <infinite-coding@outlook.de>
2020-02-11 21:56:05 +0100

    EventBus filter subscriptions via predicates

--- src/DevTools.Core.Tests/Events/EventBusTests.cs
+++ src/DevTools.Core.Tests/Events/EventBusTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using DevTools.Core.Events;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,14 +17,14 @@ namespace DevTools.Core.Tests.Events
         }
 
         [TestMethod]
-        [Timeout(100000)]
+        [Timeout(10000)]
         public void PublishWithoutSubscribersDoesNothing()
         {
             _eventBus.Publish("Payload: Leberkas Semme");
         }
 
         [TestMethod]
-        [Timeout(100000)]
+        [Timeout(10000)]
         public void PublishWithSingleSubscriberIsReceived()
         {
             bool called = false;
@@ -45,7 +46,7 @@ namespace DevTools.Core.Tests.Events
         }
         
         [TestMethod]
-        [Timeout(100000)]
+        [Timeout(10000)]
         public void PublishWithMultipleSubscriberAreReceived()
         {
             int calledCount = 0;
@@ -79,7 +80,7 @@ namespace DevTools.Core.Tests.Events
         }
         
         [TestMethod]
-        [Timeout(100000)]
+        [Timeout(10000)]
         public void SubscribeUnsubscribeSingleSubscriber()
         {
             int calledCount = 0;
@@ -104,6 +105,65 @@ namespace DevTools.Core.Tests.Events
 
             
             Assert.AreEqual(1, calledCount);
+        }
+
+        [TestMethod]
+        [Timeout(10000)]
+        public void UnsubscribeWhenNotSubscribedDoesNotFail()
+        {
+            var subscriptionId1 = _eventBus.Subscribe<object>(Console.WriteLine);
+            var subscriptionId2 = _eventBus.Subscribe<object>(Console.WriteLine);
+            
+            _eventBus.Unsubscribe<object>(subscriptionId2);
+            _eventBus.Unsubscribe<object>(subscriptionId1);
+            _eventBus.Unsubscribe<object>(subscriptionId1);
+            _eventBus.Unsubscribe<object>(subscriptionId1);
+        }
+        
+        [TestMethod]
+        [Timeout(10000)]
+        public void SubscribeWithPredicateOnlyReturnsFilteredEvents()
+        {
+            int calledCount = 0;
+
+            _eventBus.Subscribe<string>(message =>
+            {
+                Assert.Fail("Should not have been called.");
+            }, s => s.Equals("NotCalled"));
+            _eventBus.Subscribe<string>(message =>
+            {
+                calledCount++;
+            }, s => s.StartsWith("test"));
+            
+            _eventBus.Publish( "test1");
+            _eventBus.Publish( "_test");
+
+            Assert.AreEqual(1, calledCount);
+        }
+        
+        [TestMethod]
+        [Timeout(20000)]
+        public void Publish1000EventsWithPredicates()
+        {
+            int calledCountA = 0;
+            int calledCountB = 0;
+            
+            _eventBus.Subscribe<int>(message =>
+            {
+                calledCountA++;
+            }, i  => i%2 == 0 );
+            _eventBus.Subscribe<int>(message =>
+            {
+                calledCountB++;
+            }, i  => i%2 != 0 );
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _eventBus.Publish(i);
+            }
+
+            Assert.AreEqual(500, calledCountA);
+            Assert.AreEqual(500, calledCountB);
         }
     }
 }
--- src/DevTools.Core/Events/EventBus.cs
+++ src/DevTools.Core/Events/EventBus.cs
@@ -13,6 +13,7 @@ namespace DevTools.Core.Events
     //toDo: Benchmark dict vs list
     //toDo: return disposable subscription instead of guid?
     //toDo: Unsubscribe without T?
+    //ToDo: Replace T with Event - otherwise everybody could just send a string etc.
     public class EventBus
     {
         private readonly Dictionary<string, List<InternalSubscription>> _subscriptions = new Dictionary<string, List<InternalSubscription>>();
@@ -27,19 +28,27 @@ namespace DevTools.Core.Events
             {
                 foreach (var subscription in subscriptionsForTopic)
                 {
-                    subscription.Action(message);
+                    // Filter by optional predicate
+                    if (subscription.Predicate == null || subscription.Predicate(message))
+                    {
+                        subscription.Action(message);
+                    }
                 }
             }
         }
 
-        public Guid Subscribe<T>(Action<T> action)
+        public Guid Subscribe<T>(Action<T> action, Predicate<T> predicate = null)
         {
             if (action == null) throw new ArgumentNullException(nameof(action));
             var topic = typeof(T).FullName;    
             if(topic == null) throw new ArgumentNullException($"Type parameter {nameof(T)} FullName is null.");
-
+            
             var internalSubscription = new InternalSubscription(message => action((T) message));
-
+            if (predicate != null)
+            {
+                internalSubscription.Predicate = o => predicate((T)o);
+            }
+            
             if (_subscriptions.TryGetValue(topic, out var subscriptionsForTopic))
             {
                 subscriptionsForTopic.Add(internalSubscription);
@@ -79,6 +88,7 @@ namespace DevTools.Core.Events
 
             public Guid Id { get; } = Guid.NewGuid();
             public Action<object> Action { get; }
+            public Predicate<object> Predicate { get; internal set; }
         }
     }
     
