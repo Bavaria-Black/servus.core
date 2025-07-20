4d40a015e31d55aa7e3b62568239f6205353e132
Christian Dirnhofer <christian@dirnhofer.net>
2025-07-20 23:49:59 +0200

    Adds ActivitySourceRegistry +semver:feature

---
+++ src/Servus.Core/Diagnostics/ActivitySourceRegistry.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Servus.Core.Collections;
+
+namespace Servus.Core.Diagnostics;
+
+public static class ActivitySourceRegistry
+{
+    private record ExtendedEntry(string Name, ActivitySource Source);
+
+    private static readonly TypeRegistry<ExtendedEntry> Registry = new();
+
+    public static void Add<T>(string? name = null) => Create<T>(name);
+
+    private static ExtendedEntry Create<T>(string? name = null)
+    {
+        var entryName = typeof(T).Name.ToSnakeCase();
+        return new ExtendedEntry(entryName, new ActivitySource(entryName));
+    }
+
+    public static Activity? StartActivity<T>(string activityName, IWithTracing trace,
+        ActivityKind kind = ActivityKind.Consumer)
+    {
+        var entry = Registry.GetOrAdd<T>(() => Create<T>());
+        return trace.StartActivity(activityName, entry.Source, kind);
+    }
+}
