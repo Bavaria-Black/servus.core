eb9f4a42f6502ea62f462d53592baa3c741bf25b
Christian Dirnhofer <christian@dirnhofer.net>
2025-07-20 23:49:59 +0200

    Adds ActivitySourceRegistry +semver:feature

---
+++ src/Servus.Core/Collections/TypeRegistry.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace Servus.Core.Collections;
+
+public class TypeRegistry<TValue>
+{
+    protected readonly ConcurrentDictionary<Type, TValue> Dictionary = [];
+
+    public void Add<TKey>(TValue value)
+    {
+        Dictionary.AddOrUpdate(typeof(TKey), value, (_, _) => value);
+    }
+
+    public TValue Get<TKey>()
+    {
+        if (!Dictionary.TryGetValue(typeof(TKey), out var value)) throw new KeyNotFoundException();
+        return value;
+    }
+
+    public TValue GetOrAdd<TKey>(Func<TValue> factory)
+    {
+        return Dictionary.GetOrAdd(typeof(TKey), (_) => factory());
+    }
+}
