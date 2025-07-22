62cb67d39309eb38770bc15460a3ec4ffa52e804
Christian Dirnhofer <christian@dirnhofer.net>
2025-07-22 09:00:53 +0200

    added more case extensions

--- src/Servus.Core/StringExtensions.cs
+++ src/Servus.Core/StringExtensions.cs
@@ -9,18 +9,22 @@ public static partial class StringExtensions
                     @"|(?<=[a-z])(?=\d)" +                 // NameV1 → Name_V1
                     @"|(?<=\d)(?=[a-zA-Z])")]              // 1Name -> 1_name
     private static partial Regex SnakeCaseRegex();
+
+    public static string ToSnakeCase(this string value) => ToAnyCase(value, "_");
+    public static string ToDotCase(this string value) => ToAnyCase(value, ".");
+    public static string ToKebabCase(this string value) => ToAnyCase(value, "-");
     
-    public static string ToSnakeCase(this string value)
+    private static string ToAnyCase(string value, string delimiter)
     {
         if (string.IsNullOrWhiteSpace(value))
             return string.Empty;
 
-        var parts = value.Replace(" ", "_").Split('_')
+        var parts = value.Replace(" ", delimiter).Split(delimiter)
             .Where(p => !string.IsNullOrEmpty(p))
-            .Select(part => SnakeCaseRegex().Replace(part, "_").ToLower());
+            .Select(part => SnakeCaseRegex().Replace(part, delimiter).ToLower());
 
-        var result = string.Join("_", parts);
+        var result = string.Join(delimiter, parts);
         
-        return char.IsDigit(result[0]) ? "_" + result : result;
+        return char.IsDigit(result[0]) ? delimiter + result : result;
     }
 }
