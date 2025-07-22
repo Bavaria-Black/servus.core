75def18fba4b2db8784aff8c20ae32d407a51561
Christian Dirnhofer <christian@dirnhofer.net>
2025-07-22 08:49:20 +0200

    Enables ActivitySource naming via attributes

---
+++ src/Servus.Core/Diagnostics/ActivitySourceKeyAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace Servus.Core.Diagnostics;
+
+public class ActivitySourceKeyAttribute(Type sourceKey) : Attribute
+{
+    public Type SourceKey { get; } = sourceKey;
+}
--- src/Servus.Core/Diagnostics/ActivitySourceRegistry.cs
+++ src/Servus.Core/Diagnostics/ActivitySourceRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using Servus.Core.Collections;
 
 namespace Servus.Core.Diagnostics;
@@ -8,14 +9,24 @@ public static class ActivitySourceRegistry
     private record ExtendedEntry(string Name, ActivitySource Source);
 
     private static readonly TypeRegistry<ExtendedEntry> Registry = new();
+    private static readonly TypeRegistry<Type> NameRegistry = new();
 
     public static void Add<T>(string? name = null) => Create<T>(name);
 
     private static ExtendedEntry Create<T>(string? name = null)
     {
-        var entryName = typeof(T).Name.ToSnakeCase();
+        var type = typeof(T);
+        var targetType = NameRegistry.GetOrAdd<T>(() => type.GetCustomAttribute<ActivitySourceKeyAttribute>()
+            ?.SourceKey ?? type);
+
+        var entryName = name
+                        ?? targetType.GetCustomAttribute<ActivitySourceNameAttribute>()
+                            ?.ActivitySourceName
+                        ?? targetType.Name.ToSnakeCase();
+        
         return new ExtendedEntry(entryName, new ActivitySource(entryName));
     }
+
 
     public static Activity? StartActivity<T>(string activityName, IWithTracing trace,
         ActivityKind kind = ActivityKind.Consumer)
