2b584fff2202e7a43e94dd76ae4b5b3481c763b8
Christian Dirnhofer <christian@dirnhofer.net>
2025-07-24 21:02:35 +0200

    try to fix test coverage

--- src/Servus.Core.Tests/Collections/HandlerRegistryTests.cs
+++ src/Servus.Core.Tests/Collections/HandlerRegistryTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Servus.Core.Collections;
 
 namespace Servus.Core.Tests.Collections;
 
