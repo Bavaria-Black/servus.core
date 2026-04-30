4d9a62492ecbde31a28406ad8487b33a1ce2ac1d
Christian Dirnhofer <christian@dirnhofer.net>
2026-04-30 09:15:45 +0200

    introduced servus static class

---
+++ src/Servus.Core/Servus.cs
@@ -0,0 +1,9 @@
+﻿using Servus.Core.Diagnostics;
+
+namespace Servus.Core;
+
+public static class Servus
+{
+    public static readonly ServusTrace Tracing = new();
+    public static readonly ServusMetrics Metrics = new();
+}
